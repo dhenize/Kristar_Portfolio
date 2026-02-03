f48c6c69e0d8a796860f58ede5e1658c6654cbb4
dhenize <dhenizelopez@gmail.com>
2026-02-03 23:10:55 +0800

    UPDATE: Cover Page Redering, WIP: Header & Footer

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -20,7 +20,7 @@ function Homepage() {
 
 
         {/* Cover Texts */}
-        <div className="flex flex-col justify-center ml-20 space-y-10 p-15">
+        <div className="flex flex-col justify-center ml-10 space-y-10 p-10">
           {/* Header Section */}
           <div>
             <h1 className="font-['Just_Another_Hand'] text-white text-7xl md:text-8xl lg:text-9xl">
@@ -36,39 +36,39 @@ function Homepage() {
             </div>
           </div>
 
-          <div className='flex grid-cols-3 gap-5'>
-            <div className="bg-white/16 rounded-lg">
-              <p className="font-lato text-white text-lg">
+          <div className='grid grid-cols-3 gap-5'>
+            <div className="bg-white/16 rounded-lg hover:bg-white/24 transition-colors duration-300 p-2.5">
+              <p className="font-lato text-white text-lg text-center">
                 Quality Assurance
               </p>
             </div>
 
-            <div className="bg-white/16 rounded-lg">
-              <p className="font-lato text-white text-lg">
+            <div className="bg-white/16 rounded-lg hover:bg-white/24 transition-colors duration-300 p-3">
+              <p className="font-lato text-white text-lg text-center">
                 Web Developer
               </p>
             </div>
 
-            <div className="bg-white/16 rounded-lg">
-              <p className="font-lato text-white text-lg">
+            <div className="bg-white/16 rounded-lg hover:bg-white/24 transition-colors duration-300 p-3">
+              <p className="font-lato text-white text-lg text-center">
                 Mobile Developer
               </p>
             </div>
 
-            <div className="bg-white/16 rounded-lg">
-              <p className="font-lato text-white text-lg">
+            <div className="bg-white/16 rounded-lg hover:bg-white/24 transition-colors duration-300 p-3">
+              <p className="font-lato text-white text-lg text-center">
                 Development Manager
               </p>
             </div>
 
-            <div className="bg-white/16 rounded-lg">
-              <p className="font-lato text-white text-lg">
+            <div className="bg-white/16 rounded-lg hover:bg-white/24 transition-colors duration-300 p-3">
+              <p className="font-lato text-white text-lg text-center">
                 Illustrator
               </p>
             </div>
 
-            <div className="bg-white/16 rounded-lg">
-              <p className="font-lato text-white text-lg">
+            <div className="bg-white/16 rounded-lg hover:bg-white/24 transition-colors duration-300 p-3">
+              <p className="font-lato text-white text-lg text-center">
                 Multimedia Editor
               </p>
             </div>
@@ -79,7 +79,9 @@ function Homepage() {
 
 
       {/* INTRODUCTION */}
-
+      <div>
+        
+      </div>
     </div>
   )
 }
