082dea81ae8ef4d34bdadbdac913c726b37c38a3
dhenize <dhenizelopez@gmail.com>
2026-02-07 22:11:37 +0800

    UPDATE: Documents compiled, WIP: Homepage boxes

---
+++ src/components/home/education.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Education = () => {
+  return (
+    <div className='rounded-xl bg-white/10 backdrop-blur-lg border border-white/20 p-6 w-full h-[170vh] night-bg flex flex-col items-center justify-center space-y-8'>
+        
+    </div>
+  )
+}
+
+export default Education
